1ca19de407cc580c77d44c44910b20157fb46e9d
AbdullahFarooq123 <Abdullahfarooq20172018@gmail.com>
2023-02-11 11:01:40 +0500

    Final Commit

---
+++ example/lib/login_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
+import 'package:flutter_login/flutter_login.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'constants.dart';
+import 'custom_route.dart';
+import 'dashboard_screen.dart';
+import 'users.dart';
+
+class LoginScreen extends StatelessWidget {
+  static const routeName = '/auth';
+
+  const LoginScreen({Key? key}) : super(key: key);
+
+  Duration get loginTime => Duration(milliseconds: timeDilation.ceil() * 2250);
+
+  Future<String?> _loginUser(LoginData data) {
+    return Future.delayed(loginTime).then((_) {
+      if (!mockUsers.containsKey(data.name)) {
+        return 'User not exists';
+      }
+      if (mockUsers[data.name] != data.password) {
+        return 'Password does not match';
+      }
+      return null;
+    });
+  }
+
+  Future<String?> _signupUser(SignupData data) {
+    return Future.delayed(loginTime).then((_) {
+      return null;
+    });
+  }
+
+  Future<String?> _recoverPassword(String name) {
+    return Future.delayed(loginTime).then((_) {
+      if (!mockUsers.containsKey(name)) {
+        return 'User not exists';
+      }
+      return null;
+    });
+  }
+
+  Future<String?> _signupConfirm(String error, LoginData data) {
+    return Future.delayed(loginTime).then((_) {
+      return null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterLogin(
+      title: Constants.appName,
+      logo: const AssetImage('assets/images/ecorp.png'),
+      // logoTag: Constants.logoTag,
+      // titleTag: Constants.titleTag,
+      navigateBackAfterRecovery: true,
+      onConfirmRecover: _signupConfirm,
+      onConfirmSignup: _signupConfirm,
+      loginAfterSignUp: false,
+      initialAuthMode: AuthMode.login,
+      userValidator: (value) {
+        if (!value!.contains('@') || !value.endsWith('.com')) {
+          return "Email must contain '@' and end with '.com'";
+        }
+        return null;
+      },
+      passwordValidator: (value) {
+        if (value!.isEmpty) {
+          return 'Password is empty';
+        }
+        return null;
+      },
+      onLogin: (loginData) {
+        debugPrint('Login info');
+        debugPrint('Name: ${loginData.name}');
+        debugPrint('Password: ${loginData.password}');
+        return _loginUser(loginData);
+      },
+      onSubmitAnimationCompleted: () {
+        Navigator.of(context).pushReplacement(FadePageRoute(
+          builder: (context) => const DashboardScreen(),
+        ));
+      },
+      onRecoverPassword: (name) {
+        debugPrint('Recover password info');
+        debugPrint('Name: $name');
+        return _recoverPassword(name);
+        // Show new password dialog
+      },
+      showDebugButtons: true,
+    );
+  }
+}
